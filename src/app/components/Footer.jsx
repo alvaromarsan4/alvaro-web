e77c2f8628fec171ee6f5c2c15fe7d6cddaf4bb5
alvaromarsan4 <alvaro.marsan.4@educa.jcyl.es>
2026-01-14 11:52:06 +0100

    Primer commit

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from "react"; 
+
+export default class Footer extends Component{ 
+
+    render(){ 
+
+        return ( 
+
+            // Lo que tenga el pie de página
+
+            <h1>¡Hola mundo! Soy el pie de página</h1> 
+
+        ); 
+
+    } 
+
+} 
---
+++ src/app/components/Header.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from "react"; 
+
+export default class Header extends Component{ 
+
+    render(){ 
+
+        return ( 
+
+            // Lo que tenga la cabecera 
+
+            <h1>¡Hola mundo! Soy cabecera</h1> 
+
+        ); 
+
+    } 
+
+} 
